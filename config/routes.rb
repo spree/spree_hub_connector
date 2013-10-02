3a02660ba021d1934a13ea022ac51a6dbd19b89c
Pablo Cantero <pablo@pablocantero.com>
2013-10-02 17:19:01 -0300

    Removes old testing tool references

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,6 @@ Spree::Core::Engine.routes.draw do
     match 'integration/connect' => 'integration#connect'
     match 'integration/disconnect' => 'integration#disconnect'
     match 'integration/*backbone' => 'integration#show'
-
-    get 'endpoint_testing', to: redirect('/admin/endpoint_messages/new')
-
-    resources :endpoint_messages, except: [:show, :destroy] do
-      member do
-        get :clone
-      end
-      collection do
-        post :load_endpoint
-      end
-    end
   end
 
   namespace :api, :defaults => { :format => 'json' } do
