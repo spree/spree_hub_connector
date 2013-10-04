613fe01517947cc0bd4e3d43d8d17e3beb6ee3a0
John Dyer <lbrapid@gmail.com>
2013-10-04 11:12:22 -0400

    Make sure store_name is part of any newly created AuguryEnvironment

--- app/models/augury_environment.rb
+++ app/models/augury_environment.rb
@@ -1,3 +1,3 @@
 class AuguryEnvironment < ActiveRecord::Base
-  attr_accessible :store_id, :token, :environment, :user, :url
+  attr_accessible :store_id, :token, :environment, :user, :url, :store_name
 end
