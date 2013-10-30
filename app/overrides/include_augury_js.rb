768410d17f1f430c9eff7cf93a3842c096f13a73
Pablo Cantero <pablo@pablocantero.com>
2013-10-30 13:53:18 -0400

    Uses hub_client inside hub_connector in development mode avoiding long periods to reload the changes in development.

--- app/overrides/include_augury_js.rb
+++
@@ -1,7 +0,0 @@
-hub_javascript_url = ENV['HUB_JS_URL'] || "//staging.hub.spreecommerce.com/hub.min.js"
-
-Deface::Override.new(virtual_path: 'spree/layouts/admin',
-                     name: 'augury_js',
-                     insert_bottom: 'body',
-                     text: "<script type='text/javascript' src='#{hub_javascript_url}'></script>"
-                    )
