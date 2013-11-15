cd5d9a39d3ce6b6a95e93dd6579c676940492cd6
Washington Luiz <huoxito@gmail.com>
2013-11-15 11:29:55 -0300

    Add channel to api order_attributes

--- app/controllers/spree/api/orders_controller_decorator.rb
+++ app/controllers/spree/api/orders_controller_decorator.rb
@@ -7,7 +7,8 @@ Spree::Api::OrdersController.class_eval do
     [:id, :name, :product_id, :external_ref, :sku, :price, :weight, :height, :width, :depth, :is_master, :cost_price, :permalink]
   end
 
+  # NOTE we should get rid of this duplication on later versions
   def order_attributes
-    [:id, :number, :item_total, :total, :ship_total, :state, :adjustment_total, :user_id, :created_at, :updated_at, :completed_at, :payment_total, :shipment_state, :payment_state, :email, :special_instructions, :total_weight, :locked_at]
+    [:id, :number, :item_total, :total, :ship_total, :state, :adjustment_total, :user_id, :created_at, :updated_at, :completed_at, :payment_total, :shipment_state, :payment_state, :email, :special_instructions, :total_weight, :locked_at, :channel]
   end
 end
