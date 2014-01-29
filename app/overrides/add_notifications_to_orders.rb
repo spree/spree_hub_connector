2b5984edd64aa357b76b5d41fd4b2733b616fc86
John Dyer <lbrapid@gmail.com>
2014-01-29 13:51:55 -0500

    Override to add notifications from hub_client to orders/edit

---
+++ app/overrides/add_notifications_to_orders.rb
@@ -0,0 +1,26 @@
+Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
+                     :name => "add_notifications_to_orders",
+                     :insert_bottom => "[data-hook='admin_order_edit_form']",
+                     :text => %q{
+                        <div id="embed_notifications" class="row">
+                          <!-- Backbone app renders here -->
+                        </div>
+
+                        <%= javascript_tag do -%>
+                          $(function() {
+                            return Augury.notifications('<%= @order.number %>');
+                          });
+                          $(document).ajaxComplete(function() {
+                            $('.integration-image-wrapper').powerTip({
+                              placement: 'e'
+                            });
+                          });
+                        <% end %>
+
+                        <% if Rails.application.assets.find_asset('hub_client/manifest.js').nil? %>
+                          <%= javascript_include_tag '//staging.hub.spreecommerce.com/hub.min.js' %>
+                        <% else %>
+                          <%= javascript_include_tag 'hub_client/manifest.js' %>
+                        <% end %>
+                     },
+                     :disabled => false)
