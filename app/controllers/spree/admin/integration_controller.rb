4b4612838bd329bc9fd54143915c09ddb7a14bb2
John Dyer <lbrapid@gmail.com>
2014-01-29 13:32:10 -0500

    Move hub setup to Spree::Admin::BaseController so it can be used in multiple views

--- app/controllers/spree/admin/integration_controller.rb
+++ app/controllers/spree/admin/integration_controller.rb
@@ -25,23 +25,6 @@ module Spree
       end
 
       def show
-        email = 'integrator@spreecommerce.com'
-        if @integrator_user = Spree.user_class.where('email' => email).first
-          # do nothing, for now....
-        else
-          passwd = SecureRandom.hex(32)
-
-          integration_user_attrs = { 'email'                 => email,
-                                     'password'              => passwd,
-                                     'password_confirmation' => passwd }.merge(integration_user_extra_attrs)
-
-          @integrator_user = Spree.user_class.create(integration_user_attrs)
-
-          @integrator_user.spree_roles << Spree::Role.all
-          @integrator_user.generate_spree_api_key!
-        end
-
-        @environment = AuguryEnvironment.where(id: Spree::Config.augury_current_env).first
       end
 
       def integration_user_extra_attrs
